6dfe164307fb5a3c8d2bc8864d7f03a76d1f6699
adamsanderson <netghost@gmail.com>
2017-12-09 21:38:52 -0800

    Adds support for enter/esc when editing folder names
    Enter submits, escape cancels.

--- src/components/FolderEditor.js
+++ src/components/FolderEditor.js
@@ -13,6 +13,7 @@ class FolderEditor extends React.Component {
     super(props)
 
     this.handleSubmit = this.handleSubmit.bind(this)
+    this.handleKey = this.handleKey.bind(this)
   }
 
   componentDidMount() {
@@ -27,12 +28,22 @@ class FolderEditor extends React.Component {
         defaultValue={folder.title} 
         ref={el => this.input = el}
         onBlur={ this.handleSubmit }
+        onKeyUp={ this.handleKey }
       />
     )
   }
 
   handleSubmit() {
     this.props.renameFolder(this.props.folder, this.input.value)
+  }
+
+  handleKey(event) {
+    if (event.keyCode === 13) {
+      this.handleSubmit();
+    } else if (event.keyCode === 27 ) {
+      this.input.value = this.props.folder.title;
+      this.input.blur();
+    }
   }
 }
 
