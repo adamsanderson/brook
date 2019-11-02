97e28fac28d837b11dc1c93cf15f3150b73d5346
Adam Sanderson <adams@textio.com>
2019-11-01 23:21:24 -0700

    Wrap Sentry init in try/catch
    Sentry has proven difficult to upgrade and may fail silently when initializing.  This should help avoid surprises.

--- src/util/errorHandler.js
+++ src/util/errorHandler.js
@@ -2,26 +2,30 @@ import * as Sentry from '@sentry/browser'
 import ENV from './env'
 
 export function initErrorHandler(){
-  Sentry.init({
-    dsn: "https://980bd0c5a7754cb4b0c9756ff723aa69@sentry.io/1361967",
-    environment: ENV.environmentName,
-    release: `brook@${browser.runtime.getManifest().version}`,
-    whitelistUrls: [
-      /^[\w]+-extension:\//
-    ],
-    integrations: [
-      new Sentry.Integrations.RewriteFrames({
-        iteratee: (frame => {
-          if (frame.filename) {
-            // Clean URLs that include the extension id such as:
-            // moz-extension://d1eee302-f36c-7641-9595-cb9e11334238/background.html
-            frame.filename = frame.filename.replace(/\/\/[a-f0-9-]+/,"")
-          }
-          return frame
+  try {
+    Sentry.init({
+      dsn: "https://980bd0c5a7754cb4b0c9756ff723aa69@sentry.io/1361967",
+      environment: ENV.environmentName,
+      release: `brook@${browser.runtime.getManifest().version}`,
+      whitelistUrls: [
+        /^[\w]+-extension:\//
+      ],
+      integrations: [
+        new Sentry.Integrations.RewriteFrames({
+          iteratee: (frame => {
+            if (frame.filename) {
+              // Clean URLs that include the extension id such as:
+              // moz-extension://d1eee302-f36c-7641-9595-cb9e11334238/background.html
+              frame.filename = frame.filename.replace(/\/\/[a-f0-9-]+/,"")
+            }
+            return frame
+          })
         })
-      })
-    ]
-   })
+      ]
+    })
+  } catch (error) {
+    console.error("Could not initialize Sentry", error)
+  }
 }
 
 export function reportError(error, info) {
