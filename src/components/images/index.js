a42f8ffaaf62b3ef1be05afa08fdb96d16149984
Adam Sanderson <netghost@gmail.com>
2022-11-24 21:56:51 -0800

    Removes BundledImage

--- src/components/images/index.js
+++ src/components/images/index.js
@@ -1,12 +1,11 @@
 import React from 'react'
-import BundledImage from './BundledImage'
 
 import emptyPath from '../images/empty.jpg'
 import importPath from '../images/import.jpg'
 import optionsPath from '../images/options.jpg'
 import readPath from '../images/read.jpg'
 
-export const EmptyImage = (props) => <BundledImage {...props} src={emptyPath}/>
-export const ImportImage = (props) => <BundledImage {...props} src={importPath}/>
-export const OptionsImage = (props) => <BundledImage {...props} src={optionsPath}/>
-export const ReadImage = (props) => <BundledImage {...props} src={readPath}/>
+export const EmptyImage = (props) => <img {...props} src={emptyPath}/>
+export const ImportImage = (props) => <img {...props} src={importPath}/>
+export const OptionsImage = (props) => <img {...props} src={optionsPath}/>
+export const ReadImage = (props) => <img {...props} src={readPath}/>
