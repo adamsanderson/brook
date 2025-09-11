7d718f1b65fddd20c1b4d0253b3bde61909b5e8e
Adam Sanderson <netghost@gmail.com>
2025-09-11 12:11:01 -0700

    Fixes import paths
    We no longer need to rely on the browser runtime.

--- src/components/images/index.jsx
+++ src/components/images/index.jsx
@@ -5,7 +5,7 @@ import importPath from '../images/import.jpg'
 import optionsPath from '../images/options.jpg'
 import readPath from '../images/read.jpg'
 
-export const EmptyImage = (props) => <img {...props} src={browser.runtime.getURL(emptyPath)}/>
-export const ImportImage = (props) => <img {...props} src={browser.runtime.getURL(importPath)}/>
-export const OptionsImage = (props) => <img {...props} src={browser.runtime.getURL(optionsPath)}/>
-export const ReadImage = (props) => <img {...props} src={browser.runtime.getURL(readPath)}/>
+export const EmptyImage = (props) => <img {...props} src={emptyPath}/>
+export const ImportImage = (props) => <img {...props} src={importPath}/>
+export const OptionsImage = (props) => <img {...props} src={optionsPath}/>
+export const ReadImage = (props) => <img {...props} src={readPath}/>
