46970d6644cb7cf9091e3d0778768efb9e813ad0
Adam Sanderson <netghost@gmail.com>
2025-09-17 22:29:25 -0700

    Migrate url utils
    Start small…

--- src/util/url.ts
+++ src/util/url.ts
@@ -4,15 +4,17 @@
 
 
 /**
- * A leniant, but correct enough URL regular expression
+ * A lenient, but correct enough URL regular expression
  */
 const URL_REGEXP = /https?:\/\/[^\s/$.?#].[^\s]*/
 
 /**
-  Attempts to provide a reasonable "name" for a URL.
+  Attempts to provide a reasonable "name" for a URL by humanizing the last
+  segment of the URL.
+
   Invalid urls will return an empty string.
 */
-export function humanizeURL(path) {
+export function humanizeURL(path: string): string {
   try {
     const url = coerceToURL(path)
     
@@ -22,31 +24,30 @@ export function humanizeURL(path) {
     const segment = pathname.replace(/\/+$/,"").split("/").pop()
     
     // Split on non common non alphanumeric symbols and word breaks:
-    const humanized = segment.split(/[-_+\s]+/).join(" ").replace(/([a-z])([A-Z])/, "$1 $2")
+    const humanized = segment?.split(/[-_+\s]+/).join(" ").replace(/([a-z])([A-Z])/, "$1 $2")
     
-    if (humanized.length > 2) {
+    if (humanized && humanized.length > 2) {
       return humanized
     } else {
       return url.hostname
     }
     
   } catch (err) {
-    console.warn("Could not humanize: "+path+", " + err.toString())
+    console.warn("Could not humanize:", path, err?.toString())
     return path
   }
 }
 
 /**
  * Tries to return a sensible name from a url based on the host name.
- * @param {String} path 
  */
-export function humanizeHost(path) {
+export function humanizeHost(path: string): string {
   try {
     const url = coerceToURL(path)
     return url.hostname
 
   } catch (err) {
-    console.warn("Could not humanize: "+path+", "+err.toString())
+    console.warn("Could not humanize:", path, err?.toString())
     return path
   }
 }
@@ -55,22 +56,16 @@ export function humanizeHost(path) {
  * Resolves a url against the base url.  This is simply a wrapper around
  * the [URL constructor](https://developer.mozilla.org/en-US/docs/Web/API/URL/URL)
  * that will always return a String.
- * 
- * @param {String} url 
- * @param {String} base 
  */
-export function resolveUrl(url, base=window.location) {
+export function resolveUrl(url: string, base: string | URL = window.location.toString()): string {
   return new window.URL(url, base).toString()
 }
 
 /**
  * Translate feed protocol (and any others in the future) to URLs.
  * See https://en.wikipedia.org/wiki/Feed_URI_scheme for more details.
- * 
- * @param {String} uri possibly with `feed:` protocol.
- * @returns {String} url with http protocol
  */
-export function normalizeProtocol(uri) {
+export function normalizeProtocol(uri: string): string {
   return uri
     .replace(/^feed:\/\//, "http://")
     .replace(/^feed:/, "")
@@ -78,12 +73,12 @@ export function normalizeProtocol(uri) {
 
 /**
   Tries to clean up messy URLs.  For instance:
-  
+
       www.example.com => http://www.example.com
-  
+
   Throws an Error if it cannot create a URL.
 */
-function coerceToURL(path) {
+function coerceToURL(path: string): URL {
   try {
     return new window.URL(path)
   } catch (_err) {
@@ -93,9 +88,7 @@ function coerceToURL(path) {
 
 /**
  * Find urls in an arbitrary string.
- * 
- * @param {string} text 
  */
-export function findUrls(text) {
+export function findUrls(text: string): RegExpMatchArray | null {
   return text.match(URL_REGEXP)
 }
