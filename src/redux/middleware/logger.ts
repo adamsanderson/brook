6a100decf8df834017721c57975c544a0732a79b
Adam Sanderson <netghost@gmail.com>
2025-09-23 23:11:59 -0700

    Migrate logger.ts

--- src/redux/middleware/logger.ts
+++ src/redux/middleware/logger.ts
@@ -1,16 +1,18 @@
 /* eslint-disable no-console */
 
+import type { Middleware, UnknownAction } from "redux"
+
 /**
  * Logs all actions and states after they are dispatched.
  * 
  * http://redux.js.org/docs/advanced/Middleware.html
  */
-const logger = store => next => action => {
-  console.groupCollapsed(action.type)
+const logger: Middleware = store => next => (action: unknown) => {
+  console.groupCollapsed((action as UnknownAction).type)
   console.info('dispatching', action)
   const result = next(action)
   console.log('next state', store.getState())
-  console.groupEnd(action.type)
+  console.groupEnd()
   return result
 }
 
