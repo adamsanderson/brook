d86d305f7a98096318412eb6ae5ef2ea37fb8c2c
Adam Sanderson <netghost@gmail.com>
2026-02-28 22:34:22 -0800

    Migrate OpmlWriter

--- src/lib/OpmlWriter.ts
+++ src/lib/OpmlWriter.ts
@@ -1,27 +1,31 @@
 export default class OpmlWriter {
-  constructor(title) {
-    const impl = document.implementation 
+  doc: XMLDocument
+  head: HTMLHeadElement
+  body: HTMLBodyElement
+
+  constructor(title: string) {
+    const impl = document.implementation
     const doc = impl.createDocument("", "opml", null)
     this.doc = doc
-    doc.prepend(this.doc.createProcessingInstruction('xml', 'version="1.0" encoding="ISO-8859-1"')) 
-    
+    doc.prepend(this.doc.createProcessingInstruction('xml', 'version="1.0" encoding="ISO-8859-1"'))
+
     this.head = this._createHead(title)
     this.body = this._createBody()
   }
-  
+
   getRoot() {
     return this.doc.children[0]
   }
 
-  addFolder(title) {
+  addFolder(title: string) {
     return new OpmlFolder(this.doc, this.body).addFolder(title)
   }
-  
-  addFeed(title, url) {
+
+  addFeed(title: string, url: string) {
     return new OpmlFolder(this.doc, this.body).addFeed(title, url)
   }
 
-  _createHead(titleText) {
+  _createHead(titleText: string) {
     const head = this.getRoot().appendChild(this.doc.createElement("head"))
     const title = head.appendChild(this.doc.createElement("title"))
     title.appendChild(this.doc.createTextNode(titleText))
@@ -40,20 +44,23 @@ export default class OpmlWriter {
 }
 
 class OpmlFolder {
-  constructor(doc, root) {
+  doc: XMLDocument
+  root: Element
+  
+  constructor(doc: XMLDocument, root: Element) {
     this.doc = doc
     this.root = root
   }
 
-  addFolder(title) {
+  addFolder(title: string) {
     const node = this.doc.createElement("outline")
     node.setAttribute("text", title)
     this.root.appendChild(node)
-    
+
     return new OpmlFolder(this.doc, node)
   }
 
-  addFeed(title, url) {
+  addFeed(title: string, url: string) {
     const node = this.doc.createElement("outline")
     node.setAttribute("text", title)
     node.setAttribute("title", title)
